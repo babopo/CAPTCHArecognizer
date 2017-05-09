2ddb3194aa9da4f1912873956a143700f93cec57
limbo <llb123@live.com>
2017-05-10 01:26:37 +0800

    add func openfile,recognize

--- qttest.py
+++ qttest.py
@@ -4,19 +4,30 @@
 
 import sys
 
+import main
 from CAPTCHA import Ui_MainWindow
 from PyQt5 import QtWidgets, QtGui, QtCore
 
+
+Openfile = ''  # 全局变量储存打开的文件路径
 
 class recognizer(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(recognizer, self).__init__()
         self.setupUi(self)
+        self.fileopen.triggered.connect(self.showpic)
   #      self.pushButton.clicked.connect(self.showpic)  # 槽函数不用加括号
 
     def showpic(self):  # 定义槽
-        self.originalpic.setPixmap(QtGui.QPixmap('C:/Users/Limbo/Desktop/CK101CAPTCHA/0000_222E.png'))
+        global Openfile
+        file, ok = QtWidgets.QFileDialog.getOpenFileName(self, "打开", "C:/", "All Files (*);;Text Files (*.txt)")
+        self.statusbar.showMessage(file)
+        self.originalpic.setPixmap(QtGui.QPixmap(file))
+        Openfile = file
 
+    def recognize(self):
+        text = main.recognize(Openfile)
+        self.result.setText(text)
 
 if __name__ == "__main__":
 
